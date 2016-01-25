f3f8e2f36a22f9de7148224c983bd34ebc38e77e
TTAK <TTAK@TTAK-P34G-V2>
2016-01-25 09:59:59 +0100

    Ajout de commentaires

--- src/main/java/fr/umlv/java/smanipulator/ServiceSkeleton.java
+++ src/main/java/fr/umlv/java/smanipulator/ServiceSkeleton.java
@@ -13,7 +13,13 @@ public class ServiceSkeleton extends AbstractVerticle {
     @Override
     public void start() {
         Router router = Router.router(vertx);
-
+        
+        /*
+        Ici on definit les differentes URI et comment elles seront gerrés par vertx à l'aide du Handler. 
+        Ici il s'agit d'une méthode get car il n'y a pas de modifications des donnés au niveau du service.
+        Les parties de l'URI contenant des ":" sont des paramètres.
+        */
+        
         router.get("/hello/world/:name").handler(rc -> {
             String name = rc.request().getParam("name");
 
